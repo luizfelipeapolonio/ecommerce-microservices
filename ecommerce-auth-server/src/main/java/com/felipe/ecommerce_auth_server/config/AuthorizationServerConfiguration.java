ba3b067c4d5e396c88f48cfe3e2d6011483cb9ba
Luiz Felipe <luizfelipe0527@gmail.com>
2025-05-26 18:12:57 -0300

    refactor: change RSA keys generation from runtime to external generation

--- ecommerce-auth-server/src/main/java/com/felipe/ecommerce_auth_server/config/AuthorizationServerConfiguration.java
+++ ecommerce-auth-server/src/main/java/com/felipe/ecommerce_auth_server/config/AuthorizationServerConfiguration.java
@@ -5,6 +5,7 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -20,8 +21,6 @@ import org.springframework.security.oauth2.server.authorization.settings.Authori
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
@@ -29,6 +28,12 @@ import java.util.UUID;
 @Configuration
 @EnableWebSecurity
 public class AuthorizationServerConfiguration {
+
+  @Value("${jwt.key.public}")
+  private RSAPublicKey rsaPublicKey;
+
+  @Value("${jwt.key.private}")
+  private RSAPrivateKey rsaPrivateKey;
 
   @Bean
   @Order(1)
@@ -63,12 +68,8 @@ public class AuthorizationServerConfiguration {
 
   @Bean
   public JWKSource<SecurityContext> jwkSource() {
-    KeyPair keyPair = generateRsaKey();
-    RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-    RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-
-    RSAKey rsaKey = new RSAKey.Builder(publicKey)
-      .privateKey(privateKey)
+    RSAKey rsaKey = new RSAKey.Builder(rsaPublicKey)
+      .privateKey(rsaPrivateKey)
       .keyID(UUID.randomUUID().toString())
       .build();
 
@@ -84,17 +85,5 @@ public class AuthorizationServerConfiguration {
   @Bean
   public AuthorizationServerSettings authorizationServerSettings() {
     return AuthorizationServerSettings.builder().build();
-  }
-
-  private static KeyPair generateRsaKey() {
-    KeyPair keyPair;
-    try {
-      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-      keyPairGenerator.initialize(2048);
-      keyPair = keyPairGenerator.generateKeyPair();
-    } catch(Exception ex) {
-      throw new IllegalStateException(ex);
-    }
-    return keyPair;
   }
 }
