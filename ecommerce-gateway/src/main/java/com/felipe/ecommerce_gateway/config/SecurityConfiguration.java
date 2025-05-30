73844addf70fd209ebdc4df2024a3f856e282264
Luiz Felipe <luizfelipe0527@gmail.com>
2025-05-29 23:01:49 -0300

    feat: implement custom request matcher to accept bearer token from rest api call

--- ecommerce-gateway/src/main/java/com/felipe/ecommerce_gateway/config/SecurityConfiguration.java
+++ ecommerce-gateway/src/main/java/com/felipe/ecommerce_gateway/config/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package com.felipe.ecommerce_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,6 +19,18 @@ public class SecurityConfiguration {
     return http
       .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
       .oauth2Login(Customizer.withDefaults())
+      .securityMatcher(bearerAwareSecurityMatcher)
       .build();
   }
+
+  // Custom security matcher - Rest api call support
+  // if the request has a Bearer token, skip security entirely, allowing the request to
+  // be forwarded
+  private final ServerWebExchangeMatcher bearerAwareSecurityMatcher = exchange -> {
+    String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+    if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+      return MatchResult.notMatch(); // Skip security
+    }
+    return MatchResult.match(); // Apply security (OAuth2 Login)
+  };
 }
