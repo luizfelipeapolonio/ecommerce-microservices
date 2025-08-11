9e04d28a74ac3cd7d234415289da80589d7efb63
Luiz Felipe <luizfelipe0527@gmail.com>
2025-08-11 20:53:46 -0300

    style: remove unused commented code

--- ecommerce-upload-service/src/main/java/com/felipe/ecommerce_upload_service/dtos/ImageDTO.java
+++ ecommerce-upload-service/src/main/java/com/felipe/ecommerce_upload_service/dtos/ImageDTO.java
@@ -3,9 +3,6 @@ package com.felipe.ecommerce_upload_service.dtos;
 import com.felipe.ecommerce_upload_service.persistence.entities.Image;
 import com.felipe.ecommerce_upload_service.utils.ImageUtils;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public record ImageDTO(String id,
                        String name,
                        String path,
@@ -16,8 +13,6 @@ public record ImageDTO(String id,
                        String productId,
                        String createdAt,
                        String updatedAt) {
-  //private static final double UNIT_BYTE = Math.pow(1000, -2);
-
   public ImageDTO(Image image) {
     this(
       image.getId().toString(),
@@ -32,10 +27,4 @@ public record ImageDTO(String id,
       image.getUpdatedAt().toString()
     );
   }
-
-//  private static String convertByteToMegabyte(long bytes) {
-//    double convertedMegabytes = UNIT_BYTE * bytes;
-//    BigDecimal bigDecimalMegabytes = new BigDecimal(convertedMegabytes).setScale(2, RoundingMode.DOWN);
-//    return String.format("%s MB", bigDecimalMegabytes.toString());
-//  }
 }
