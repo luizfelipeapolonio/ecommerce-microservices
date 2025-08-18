7c5bd967dba329ac190c64af6a86791ee632cb2f
Luiz Felipe <luizfelipe0527@gmail.com>
2025-08-18 16:22:58 -0300

    refactor: adjust authorization rules

--- ecommerce-upload-service/src/main/java/com/felipe/ecommerce_upload_service/config/security/SecurityConfiguration.java
+++ ecommerce-upload-service/src/main/java/com/felipe/ecommerce_upload_service/config/security/SecurityConfiguration.java
@@ -2,13 +2,20 @@ package com.felipe.ecommerce_upload_service.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,19 +29,32 @@ public class SecurityConfiguration {
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(authorize -> authorize
         .requestMatchers(HttpMethod.GET, DOCS_WHITELIST).permitAll()
-        .anyRequest().hasAuthority("ROLE_ADMIN"))
+        .anyRequest().hasAnyAuthority("ROLE_ADMIN", "SCOPE_admin"))
       .oauth2ResourceServer(oauth2 -> oauth2
         .jwt(jwt -> jwt.jwtAuthenticationConverter(this.jwtAuthenticationConverter())))
       .build();
   }
 
   private JwtAuthenticationConverter jwtAuthenticationConverter() {
-    JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-    grantedAuthoritiesConverter.setAuthorityPrefix("");
-    grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
-
     JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter());
     return jwtAuthenticationConverter;
+  }
+
+  private Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter() {
+    // Use 'scope' or 'scp' claim (the default) to extract authorities
+    JwtGrantedAuthoritiesConverter defaultAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    // Use 'authorities' claim to extract authorities
+    JwtGrantedAuthoritiesConverter customAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    customAuthoritiesConverter.setAuthorityPrefix("");
+    customAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+
+    return jwt -> {
+      List<GrantedAuthority> authorities = new ArrayList<>();
+      authorities.addAll(defaultAuthoritiesConverter.convert(jwt));
+      authorities.addAll(customAuthoritiesConverter.convert(jwt));
+      return authorities;
+    };
   }
 }
