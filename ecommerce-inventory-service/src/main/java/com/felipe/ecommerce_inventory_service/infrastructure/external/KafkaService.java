c04e182ee8c9bace377d50590034a9ab178a8bb6
Luiz Felipe <luizfelipe0527@gmail.com>
2025-11-25 22:38:45 -0300

    feat: add logs for removing expired promotions functionality

--- ecommerce-inventory-service/src/main/java/com/felipe/ecommerce_inventory_service/infrastructure/external/KafkaService.java
+++ ecommerce-inventory-service/src/main/java/com/felipe/ecommerce_inventory_service/infrastructure/external/KafkaService.java
@@ -3,12 +3,15 @@ package com.felipe.ecommerce_inventory_service.infrastructure.external;
 import com.felipe.ecommerce_inventory_service.infrastructure.persistence.entities.ProductEntity;
 import com.felipe.ecommerce_inventory_service.infrastructure.persistence.repositories.ProductRepository;
 import com.felipe.kafka.ExpiredPromotionKafkaDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaService {
   private final ProductRepository productRepository;
+  private final Logger logger = LoggerFactory.getLogger(KafkaService.class);
 
   public KafkaService(ProductRepository productRepository) {
     this.productRepository = productRepository;
@@ -16,8 +19,10 @@ public class KafkaService {
 
   @KafkaListener(topics = "expired-promotion", groupId = "promotion-group")
   void removeExpiredPromotionFromProducts(ExpiredPromotionKafkaDTO expiredPromotionDTO) {
+    this.logger.info("=== Removing expired promotions ===");
     this.productRepository.findAllByPromotionId(expiredPromotionDTO.promotionId())
       .forEach(product -> {
+        this.logger.info("Removing promotion \"{}\" from product \"{}\"", expiredPromotionDTO.promotionId(), product.getName());
         final ProductEntity updatedEntity = ProductEntity.mutate(product)
           .withDiscount(false)
           .promotionId(null)
