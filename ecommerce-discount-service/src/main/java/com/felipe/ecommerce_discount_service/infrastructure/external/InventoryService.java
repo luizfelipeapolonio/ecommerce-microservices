cc97f3029a80728ea768d9269e015a1a9b50958d
Luiz Felipe <luizfelipe0527@gmail.com>
2026-01-09 18:59:36 -0300

    feat: add Circuitbreakers support

--- ecommerce-discount-service/src/main/java/com/felipe/ecommerce_discount_service/infrastructure/external/InventoryService.java
+++ ecommerce-discount-service/src/main/java/com/felipe/ecommerce_discount_service/infrastructure/external/InventoryService.java
@@ -1,13 +1,21 @@
 package com.felipe.ecommerce_discount_service.infrastructure.external;
 
 import com.felipe.ecommerce_discount_service.infrastructure.dtos.promotion.PromotionAppliesToDTOImpl;
+import com.felipe.ecommerce_discount_service.infrastructure.exceptions.InventoryServiceException;
 import com.felipe.ecommerce_discount_service.infrastructure.persistence.entities.PromotionEntity;
 import com.felipe.response.ResponsePayload;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 
 import java.net.URI;
 import java.util.List;
@@ -22,20 +30,39 @@ public class InventoryService {
   @Value("${services.inventory-service.url}")
   private String inventoryServiceUrl;
   private static final String CLIENT_REGISTRATION_ID = "ecommerce-discount-service";
+  private final Logger logger = LoggerFactory.getLogger(InventoryService.class);
 
   public InventoryService(RestClient restClient) {
     this.restClient = restClient;
   }
 
+  @CircuitBreaker(name = "inventoryServiceCircuitBreaker", fallbackMethod = "fallback")
+  @RateLimiter(name = "inventoryServiceCircuitBreaker", fallbackMethod = "fallback")
   public ResponsePayload<Map<String, Integer>> applyPromotion(PromotionRequest promotionRequest) {
-    return this.restClient
-      .post()
-      .uri(URI.create(this.inventoryServiceUrl))
-      .attributes(clientRegistrationId(CLIENT_REGISTRATION_ID))
-      .contentType(MediaType.APPLICATION_JSON)
-      .body(promotionRequest)
-      .retrieve()
-      .body(new ParameterizedTypeReference<>() {});
+    try {
+      return this.restClient
+        .post()
+        .uri(URI.create(this.inventoryServiceUrl))
+        .attributes(clientRegistrationId(CLIENT_REGISTRATION_ID))
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(promotionRequest)
+        .retrieve()
+        .body(new ParameterizedTypeReference<>() {
+        });
+    } catch(RestClientException ex) {
+      this.logger.error("RestClient error in applyPromotion: {}", ex.getMessage());
+      throw new InventoryServiceException("Ocorreu um erro ao se comunicar com a aplicação");
+    }
+  }
+
+  private ResponsePayload<Map<String, Integer>> fallback(PromotionRequest promotionRequest, CallNotPermittedException ex) {
+    this.logger.error("Circuitbreaker fallback -> {}", ex.getMessage());
+    throw ex;
+  }
+
+  private ResponsePayload<Map<String, Integer>> fallback(PromotionRequest promotionRequest, RequestNotPermitted ex) {
+    this.logger.error("RateLimiter fallback -> {}", ex.getMessage());
+    throw ex;
   }
 
   public static class PromotionRequest {
--- ecommerce-discount-service/src/main/java/com/felipe/ecommerce_discount_service/infrastructure/presentation/ExceptionControllerAdvice.java
+++ ecommerce-discount-service/src/main/java/com/felipe/ecommerce_discount_service/infrastructure/presentation/ExceptionControllerAdvice.java
@@ -6,9 +6,12 @@ import com.felipe.ecommerce_discount_service.core.application.exceptions.Invalid
 import com.felipe.ecommerce_discount_service.core.application.exceptions.InvalidPromotionAppliesTargetException;
 import com.felipe.ecommerce_discount_service.core.application.exceptions.InvalidPromotionScopeException;
 import com.felipe.ecommerce_discount_service.infrastructure.exceptions.CreatePromotionDTOValidationException;
+import com.felipe.ecommerce_discount_service.infrastructure.exceptions.InventoryServiceException;
 import com.felipe.response.CustomValidationErrors;
 import com.felipe.response.ResponsePayload;
 import com.felipe.response.ResponseType;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -121,6 +124,47 @@ public class ExceptionControllerAdvice {
       .type(ResponseType.ERROR)
       .code(HttpStatus.INTERNAL_SERVER_ERROR)
       .message("Ocorreu um erro interno do servidor. Por favor, tente mais tarde")
+      .build();
+  }
+
+  @ExceptionHandler(RequestNotPermitted.class)
+  @ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
+  public ResponsePayload<Void> handleRequestNotPermittedException() {
+    return new ResponsePayload.Builder<Void>()
+      .type(ResponseType.ERROR)
+      .code(HttpStatus.TOO_MANY_REQUESTS)
+      .message("Muitas requisições foram feitas em um determinado período de tempo. Por favor, tente novamente mais tarde")
+      .build();
+  }
+
+  @ExceptionHandler(CallNotPermittedException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ResponsePayload<Void> handleCallNotPermittedException() {
+    return new ResponsePayload.Builder<Void>()
+      .type(ResponseType.ERROR)
+      .code(HttpStatus.INTERNAL_SERVER_ERROR)
+      .message("Ocorreu um erro ao se comunicar com o servidor")
+      .build();
+  }
+
+  @ExceptionHandler(InventoryServiceException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ResponsePayload<Void> handleInventoryServiceException(InventoryServiceException ex) {
+    return new ResponsePayload.Builder<Void>()
+      .type(ResponseType.ERROR)
+      .code(HttpStatus.INTERNAL_SERVER_ERROR)
+      .message(ex.getMessage())
+      .build();
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ResponsePayload<Void> handleUncaughtException(Exception ex) {
+    this.logger.error("Uncaught exception handler: {}", ex.getMessage());
+    return new ResponsePayload.Builder<Void>()
+      .type(ResponseType.ERROR)
+      .code(HttpStatus.INTERNAL_SERVER_ERROR)
+      .message("Ocorreu um erro interno do servidor! Por favor, tente mais tarde")
       .build();
   }
 }
