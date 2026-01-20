d0f751f1f686cad7c9390b657fda6f50825e6d07
Luiz Felipe <luizfelipe0527@gmail.com>
2026-01-20 18:19:23 -0300

    change: add fields to table

--- ecommerce-cart-service/src/main/resources/db/migration/V2__create_table_cart_items.sql
+++ ecommerce-cart-service/src/main/resources/db/migration/V2__create_table_cart_items.sql
@@ -1,9 +1,14 @@
 CREATE TABLE cart_items (
-    id         BIGSERIAL NOT NULL PRIMARY KEY,
-    product_id UUID      NOT NULL,
-    cart_id    UUID      NOT NULL,
-    quantity   INTEGER   NOT NULL,
-    added_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id              BIGSERIAL    NOT NULL PRIMARY KEY,
+    product_id      UUID         NOT NULL,
+    product_name    VARCHAR(300) NOT NULL,
+    thumbnail_image TEXT         NOT NULL,
+    unit_price      NUMERIC      NOT NULL,
+    discount_type   VARCHAR(12),
+    discount_value  VARCHAR(50),
+    quantity        INTEGER      NOT NULL,
+    cart_id         UUID         NOT NULL,
+    added_at        TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_carts FOREIGN KEY (cart_id) REFERENCES carts(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
