0ff2926533b881fa1ad79c737696355283483eef
Luiz Felipe <luizfelipe0527@gmail.com>
2026-04-17 19:13:06 -0300

    build: add ecommerce-delivery-service module and update dependencies

---
+++ ecommerce-delivery-service/src/test/java/com/felipe/ecommerce_delivery_service/EcommerceDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.ecommerce_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
